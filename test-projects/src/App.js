3800077de95e4a15277ff5dd201d5d954b53fbfc
Fathima farhath <78297545+fathimafarhath@users.noreply.github.com>
2021-10-01 12:50:20 +0530

    Add files via upload

---
+++ test-projects/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from './components/Form';
+import './App.css';
+
+const App = () => {
+  return(
+     <div>
+       <Form />
+       
+
+     </div>
+  )
+  
+}
+
+export default App
+
